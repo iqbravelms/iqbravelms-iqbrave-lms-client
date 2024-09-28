57f9b4791bab66a4f96ecc4d5ebd53c359a60b68
unknown <dmahesh9810@gmail.com>
2024-09-28 10:20:21 +0530

    watch lesson page assignment section update

--- src/views/WatchLesson.vue
+++ src/views/WatchLesson.vue
@@ -6,41 +6,110 @@
         </div>
         <div v-else class="p-24">
             <div
-                class=" w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                class="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                 <button aria-current="true" type="button"
                     class="cursor-default w-full px-4 py-2 font-medium text-left rtl:text-right text-white bg-blue-700 border-b border-gray-200 rounded-t-lg focus:outline-none dark:bg-gray-800 dark:border-gray-600">
-                    Modules : {{ moduleName }} <br> Topic : {{ data.topic }}
+                    Modules: {{ moduleName }} <br> Topic: {{ data.topic }}
                 </button>
 
                 <div class="flex justify-center p-16">
                     <div class="relative w-full" style="padding-top: 56.25%;">
-                        <iframe class="absolute top-0 left-0 w-full h-full border-0" :src="data.link"
+                        <iframe v-if="data.link" class="absolute top-0 left-0 w-full h-full border-0" :src="data.link"
                             title="YouTube Video"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen>
                         </iframe>
                     </div>
                 </div>
+
                 <div class="p-12">
-                    <ul class="text-base"><strong>Note : </strong>
+                    <ul class="text-base"><strong>Note:</strong>
                         <ul
-                            class="w-full px-4 py-2 font-medium text-left rtl:text-right border-b border-gray-200 cursor-default  bg-gray-100 text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-                            {{ data.Note }} </ul>
+                            class="w-full px-4 py-2 font-medium text-left rtl:text-right border-b border-gray-200 cursor-default bg-gray-100 text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+                            {{ data.Note }}
+                        </ul>
                     </ul>
                 </div>
 
-
                 <div class="pl-12 pb-12 pr-12">
-
-                    <p class="text-base"><strong>Steps </strong></p>
+                    <p class="text-base"><strong>Steps</strong></p>
                     <ul
-                        class="w-full px-4 py-2 font-medium text-left rtl:text-right border-b border-gray-200 cursor-default  bg-gray-100 text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+                        class="w-full px-4 py-2 font-medium text-left rtl:text-right border-b border-gray-200 cursor-default bg-gray-100 text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
                         <li v-for="(step, index) in data.steps" :key="step.step_id">
                             Step {{ index + 1 }}: {{ step.description }}
                         </li>
                     </ul>
                 </div>
 
+                <div class="pl-12 pb-12 pr-12">
+                    <a href="#"
+                        class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow cursor-default">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Assignment
+                            Section</h5>
+                        <button v-if="showAssignmentButton" type="button"
+                            class="text-white hover:text-white border bg-yellow-400 hover:bg-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+                            @click.prevent="getAssignments(data.id)">I want Assignment</button>
+                        <div v-if="loadingAssignment">Loading assignments...</div>
+                        <div v-else-if="errorAssignment">
+                            <h2>Error: {{ errorAssignment }}</h2>
+                        </div>
+                        <div v-else class="p-12">
+                            <div v-if="assignments && assignments.length">
+                                <div v-for="(assignment, index) in assignments" :key="assignment.assignment_id"
+                                    class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow cursor-default mt-4">
+
+                                    <!-- Assignment Heading -->
+                                    <h3 class="text-xl font-semibold text-gray-900 mb-4">Assignment {{ index + 1 }}</h3>
+
+                                    <!-- Download Structure Section -->
+                                    <div class="flex justify-between items-center mb-4">
+                                        <p class="text-gray-700 font-medium"><strong>Download structure:</strong></p>
+                                        <div class="flex space-x-4">
+                                            <a v-if="extractDriveId(assignment.structure)"
+                                                :href="'https://drive.google.com/uc?export=download&id=' + extractDriveId(assignment.structure)"
+                                                download
+                                                class="text-white bg-yellow-400 hover:bg-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5">
+                                                Download structure File
+                                            </a>
+                                            <a :href="assignment.structure" target="_blank"
+                                                class="text-white bg-blue-400 hover:bg-blue-500 font-medium rounded-lg text-sm px-5 py-2.5">
+                                                Open Structure File
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                    <!-- Assignment Files Section -->
+                                    <div class="mt-4">
+                                        <h4 class="text-lg font-semibold text-gray-800 mb-3">Additional Files:</h4>
+                                        <ul class="space-y-4">
+                                            <li v-for="file in assignment.files" :key="file.file_id"
+                                                class="flex justify-between items-center">
+                                                <p class="text-gray-700 font-medium"><strong>File Name:</strong> {{
+                                                    file.AssignmentName }}</p>
+
+                                                <!-- File Download Link -->
+                                                <div class="flex space-x-4">
+                                                    <a :href="file.Link" target="_blank"
+                                                        class="text-white bg-yellow-400 hover:bg-yellow-500 font-medium rounded-lg text-sm px-5 py-2.5">
+                                                        View File
+                                                    </a>
+                                                    <a v-if="extractDriveId(file.Link)"
+                                                        :href="'https://drive.google.com/uc?export=download&id=' + extractDriveId(file.Link)"
+                                                        download
+                                                        class="text-white bg-blue-400 hover:bg-blue-500 font-medium rounded-lg text-sm px-5 py-2.5">
+                                                        Download File
+                                                    </a>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div v-else class="text-gray-600">No assignments available.</div>
+                        </div>
+
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -52,45 +121,80 @@ import { ref, onMounted } from 'vue';
 
 export default {
     setup() {
-        const data = ref({}); // Initialize data as an object
+        const data = ref({});
+        const assignments = ref([]);
         const loading = ref(true);
+        const loadingAssignment = ref(false);
         const error = ref(null);
+        const errorAssignment = ref(null);
         const moduleName = localStorage.getItem('moduleName');
+        const token = localStorage.getItem('token');
+        const showAssignmentButton = ref(true);
 
         const loadLesson = async () => {
-            const token = localStorage.getItem('token'); // Retrieve the token from localStorage
-            const topicId = localStorage.getItem('topicId'); // Retrieve the topicId from localStorage
+            const topicId = localStorage.getItem('topicId');
             try {
                 const response = await axios.get(`http://localhost:8000/api/lesson/${topicId}`, {
                     headers: {
                         'Content-Type': 'application/x-www-form-urlencoded',
-                        'Authorization': `Bearer ${token}`  // Ensure token is present here
+                        'Authorization': `Bearer ${token}`
                     }
                 });
-                data.value = response.data.lesson; // Adjust based on your actual API response
-                console.log(response.data);
+                data.value = response.data.lesson;
             } catch (err) {
-                error.value = err.message; // Set error message
-                console.error(err); // Log the error for debugging
+                error.value = err.response ? err.response.data.message : err.message;
             } finally {
-                loading.value = false; // Set loading to false after fetching
+                loading.value = false;
             }
         };
 
+        const getAssignments = async (lessonId) => {
+            loadingAssignment.value = true;
+            errorAssignment.value = null;
+            try {
+                const response = await axios.get(`http://localhost:8000/api/assignments/${lessonId}`, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                assignments.value = response.data.assignments;
+                showAssignmentButton.value = false;
+            } catch (err) {
+                errorAssignment.value = err.response ? err.response.data.message : err.message;
+            } finally {
+                loadingAssignment.value = false;
+            }
+        };
+
+        // Function to extract Google Drive file ID from the URL
+        const extractDriveId = (url) => {
+            const regex = /\/d\/(.*?)\//;
+            const match = url ? url.match(regex) : null;
+            return match ? match[1] : '';
+        };
+
         onMounted(() => {
-            loadLesson(); // Load lesson when the component is mounted
+            loadLesson();
         });
 
         return {
             data,
+            assignments,
             loading,
             error,
             moduleName,
+            loadingAssignment,
+            errorAssignment,
+            token,
+            getAssignments,
+            showAssignmentButton,
+            extractDriveId // Return the function to use it in the template
         };
     },
-
     name: 'AppTopic',
-}
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+/* Your styles here */
+</style>
